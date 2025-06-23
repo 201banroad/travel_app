bba8a2bff6f9b312fec419fe21dd56078cb1f582
201banroad <hiroto01021202@icloud.com>
2025-06-23 15:48:04 +0900

    アカウント削除機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
+  validates :name,
+    presence: true,
+    length: { maximum: 13 }, # 13文字以内
+    uniqueness: true        # 他のユーザーと同じ名前はダメ
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :spots # ← ユーザーは投稿をたくさん持ってるよ
+
+  # ユーザーを削除したときに、その人の投稿も全部一緒に削除される
+  has_many :spots, dependent: :destroy
+ 
 end
