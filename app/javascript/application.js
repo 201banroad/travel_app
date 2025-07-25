d4fb393e39e4021ccc0f6a30b570c2e692012d55
201banroad <hiroto01021202@icloud.com>
2025-07-25 15:39:13 +0900

    ふせんGridで一応整えた

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,26 @@
 
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+// const container = document.querySelector('.sticky-container');
+// const notes = document.querySelectorAll('.sticky-note');
+
+// notes.forEach((note, i) => {
+//   const containerWidth = container.clientWidth;
+//   const containerHeight = container.clientHeight;
+
+//   const noteWidth = 170;
+//   const noteHeight = 150;
+
+//   const maxLeft = containerWidth - noteWidth;
+//   const maxTop = containerHeight - noteHeight;
+
+//   const randomLeft = Math.random() * maxLeft;
+//   const randomTop = Math.random() * maxTop;
+
+//   note.style.left = `${randomLeft}px`;
+//   note.style.top = `${randomTop}px`;
+
+//   note.style.transform = `rotate(${(Math.random() - 0.5) * 10}deg)`; // -5〜+5度の傾き
+// });
