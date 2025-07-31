0565e5e8abd8ff9ed3bd24dfd61eb3902bd96730
201banroad <hiroto01021202@icloud.com>
2025-07-31 18:24:01 +0900

    投稿画面の改行、文字数のバリテーション追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,8 +1,18 @@
 class Spot < ApplicationRecord
     # タイトル、詳細、写真は必須にするためのバリデーション処理
-    validates :title, presence: { message: "タイトルは必須です" }
-    validates :description, presence: { message: "魅力は必須です" }
+    validates :title, presence: { message: "タイトルは必須です" },
+    length: { maximum: 30, message: "は30文字以内で入力してください" }
+
+    validates :description, presence: { message: "魅力は必須です" },
+    length: { maximum: 700, message: "は700文字以内で入力してください" }
+
+    validates :location,
+    length: { maximum: 15, message: "は15文字以内で入力してください" }
     validates :images, presence: { message: "写真は必須です" }
+
+    validate :description_line_limit
+
+
 
     # 画像以外投稿できないバリテーション
     validate :images_must_be_images
@@ -11,7 +21,6 @@ class Spot < ApplicationRecord
     has_many_attached :images
 
     belongs_to :user, optional: true
-    
     # いいねの設定
     has_many :likes, dependent: :destroy
     has_many :liked_users, through: :likes, source: :user
@@ -23,6 +32,16 @@ class Spot < ApplicationRecord
       unless image.content_type.starts_with?("image/")
         errors.add(:images, "は画像ファイルのみアップロードできます")
       end
+    end
+  end
+
+
+  def description_line_limit
+    return if description.blank?
+
+    max_lines = 20
+    if description.count("\n") >= max_lines
+      errors.add(:description, "は#{max_lines}行以内で入力してください")
     end
   end
 
