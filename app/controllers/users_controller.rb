f7c08de7ae7d1b46f9fb295d15c9f29d1877a544
201banroad <hiroto01021202@icloud.com>
2026-02-07 04:14:03 +0900

    いいね欄と投稿を新着順に並べた、リファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,13 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @spots = @user.spots.order(created_at: :desc)
+        @liked_spots = @user.likes.order(created_at: :desc).map(&:spot)
     end
     
     def mypage
         @user = current_user
         @spots = @user.spots.order(created_at: :desc)
+        @liked_spots = @user.likes.order(created_at: :desc).map(&:spot)
         render :show
     end
 
