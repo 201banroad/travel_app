d8c81a8d1bde0faa25b752c7a66842bd14b036b3
201banroad <hiroto01021202@icloud.com>
2026-02-09 21:16:15 +0900

    画像の解像度指定すると重くて落ちるので、画像本来の解像度を反映

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -1,2 +1,13 @@
 module SpotsHelper
+  def image_dimensions(image)
+    if image.blob.metadata['identified'] && image.blob.metadata['width'] && image.blob.metadata['height']
+      width = image.blob.metadata['width']
+      height = image.blob.metadata['height']
+      "#{width}x#{height}"
+    end
+  end
+
+  def image_alt_text(image)
+    image_dimensions(image) || "image"
+  end
 end
